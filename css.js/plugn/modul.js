4b78b8154d851840865633cfa09513eb6ac1dbbe
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-05-26 14:07:23 +0500

    file

---
+++ css.js/plugn/modul.js
@@ -0,0 +1,18 @@
+// function creteModal(options) {
+// const modal=document.createElement(tagName 'div')
+// modal.clasList.add('vmodal')
+
+
+// `)
+// }
+// $.module=function (options) {
+
+//     return{
+//   open() {}, 
+//   close() {},
+//   destroy(){},      
+
+//     }
+// }
+
+
